06c2bea5084e2088bb878be515499d2116232d87
AnatoliiG <avgolovenko@mai.education>
2023-10-28 17:52:59 +0300

    another main with array

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <pthread.h>
 #include <string>
-#include <vector>
 
 typedef struct
 {
-    std::vector<int> &fst, &scnd;
+    int *fst, scnd;
     int size;
 } arguments;
 
-void *sm(std::vector<int> &ms1, std::vector<int> &ms2, int size)
-{
-    for (int i = 0; i < size; ++i)
-    {
-        ms1[i] += ms2[i];
-        ms2[i] = 0;
-    }
-}
+// void *sm(std::vector<int> &ms1, std::vector<int> &ms2, int size)
+// {
+//     for (int i = 0; i < size; ++i)
+//     {
+//         ms1[i] += ms2[i];
+//         ms2[i] = 0;
+//     }
+// }
 
-void thread_work(std::vector<std::vector<int>> &data, int threads_count, int count_vectors, int ln_of_one_vector)
+void thread_work(int &data, int threads_count, int count_vectors, int ln_of_one_vector)
 {
     pthread_t threads[threads_count];
     int step = 1, created = 0, i = 0;
@@ -26,7 +25,7 @@ void thread_work(std::vector<std::vector<int>> &data, int threads_count, int cou
     {
         for (int i = 0; i < count_vectors; i += 2 * step)
         {
-            pthread_create(&threads[created], NULL, sm, data[i], data[i + 1], ln_of_one_vector);
+            // pthread_create(&threads[created], NULL, sm, data[i], data[i + 1], ln_of_one_vector);
         }
         // if (count_vectors % 2 == 0)
         // {
@@ -49,7 +48,7 @@ int main(int argc, char *argv[])
     std::cout << "What size of vector would you like?" << std::endl;
     std::cin >> ln_of_one_vector;
 
-    std::vector<std::vector<int>> data(count_vectors, std::vector<int>(ln_of_one_vector));
+    int data[count_vectors][ln_of_one_vector];
 
     for (int i = 0; i < count_vectors; ++i)
     {
